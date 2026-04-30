de1e4529f7308e5963b02a88506c023de4ff29ad
Alex <takayshvilialeksandr@gmail.com>
2026-04-30 18:13:47 +0300

    feat: define nginx container with port mapping

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
 
+resource "docker_container" "nginx" {
+  name  = "nginx-test"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
