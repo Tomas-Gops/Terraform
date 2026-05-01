e6b47fe8917bbfe05cc5f7e2c7ec9bc50f4eacbf
Alex <takayshvilialeksandr@gmail.com>
2026-05-01 17:06:48 +0300

    feat: add docker network for container communication

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ terraform {
     }
   }
 }
+resource "docker_network" "app_network" {
+  name = "app-network"
+}
 
 provider "docker" {}
 resource "docker_image" "nginx" {
@@ -21,13 +24,15 @@ resource "docker_container" "nginx" {
     external = var.external_port
   }
   volumes {
-    host_path      = "${path.module}/nginx/default.conf"
+    host_path      = "/home/alex/Terraform/nginx/default.conf"
     container_path = "/etc/nginx/nginx.conf"
   }
 }
+
 resource "docker_image" "echo" {
   name = "hashicorp/http-echo"
 }
+
 resource "docker_container" "echo" {
   name  = "echo-test"
   image = docker_image.echo.image_id
