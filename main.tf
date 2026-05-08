a49fdf34bae180f2c400c66febb01d7bacc19c49
Alex <takayshvilialeksandr@gmail.com>
2026-05-08 17:22:23 +0300

    feat: generate nginx config dynamically from backend containers

--- main.tf
+++ main.tf
@@ -4,33 +4,20 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0"
     }
+    local = {
+      source = "hashicorp/local"
+    }
   }
 }
 resource "docker_network" "app_network" {
   name = "app-network"
 }
 
-provider "docker" {}
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
-
-resource "docker_container" "nginx" {
-  name  = "nginx"
-  image = docker_image.nginx.image_id
-
-  networks_advanced {
-    name = docker_network.app_network.name
-  }
-  ports {
-    internal = 80
-    external = var.external_port
-  }
-  volumes {
-    host_path = "/home/alex/Terraform/nginx/default.conf"
-    container_path = "/etc/nginx/conf.d/conf_one.conf"
-  }
-
+locals {
+  backend_container_names = [
+    for container in docker_container.echo :
+    container.name
+  ]
 }
 
 resource "docker_image" "echo" {
@@ -49,3 +36,37 @@ resource "docker_container" "echo" {
   }
 }
 
+resource "local_file" "nginx_config" {
+  content = templatefile(
+    "${path.module}/templates/nginx.conf.tftpl",
+    {
+      backend_containers = local.backend_container_names
+    }
+  )
+
+  filename = "${path.module}/nginx/default.conf"
+}
+
+provider "docker" {}
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+  depends_on = [local_file.nginx_config]
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx"
+  image = docker_image.nginx.image_id
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+  ports {
+    internal = 80
+    external = var.external_port
+  }
+  volumes {
+    host_path = "/home/alex/Terraform/nginx/default.conf"
+    container_path = "/etc/nginx/conf.d/default.conf"
+  }
+
+}
